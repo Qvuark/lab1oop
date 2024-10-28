3b2200476f67a8bd6de775dbea1e9e497a9b3cfe
Qvuark <roma18902015@gmail.com>
2024-10-28 11:01:15 +0200

    done but without tests

---
+++ lab1 oop/lab1 oop.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <cmath>
+
+class Triangle 
+{
+private:
+    int a, b, c;
+public:
+    Triangle(double sideA = 1.0, double sideB = 1.0, double sideC = 1.0) 
+    {
+        a = sideA;
+        b = sideB;
+        c = sideC;
+    }
+    int operator[](int index) const
+    {
+        switch (index)
+        {
+        case 0: return a;
+        case 1: return b;
+        case 2: return c;
+        default:
+            std::cout << "Invalid index. Use 0 for a, 1 for b, 2 for c."<<std::endl;
+            return -1;
+        }
+    }
+    bool isValid() const 
+    {
+        return (a + b > c) && (a + c > b) && (b + c > a);
+    }
+    double getPerimeter() const 
+    {
+        if (isValid()) 
+        {
+            return a + b + c;
+        }
+        else 
+        {
+            std::cout << "Triangle doesn't exist";
+            return 0;
+        }
+    }
+    double getArea() const 
+    {
+        if (isValid()) 
+        {
+            double s = getPerimeter() / 2;
+            return sqrt(s * (s - a) * (s - b) * (s - c));
+        }
+        else 
+        {
+            std::cout << "Triangle doesnt exist";
+            return 0;
+        }
+    }
+    void inputSides() 
+    {
+        std::cout<<"Enter a,b,c: ";
+        bool isInputRight = false;
+        do
+        {
+            std::cin >> a >> b >> c;
+            if (std::cin.fail())
+            {
+                std::cin.clear();
+                std::cin.ignore(32767, '\n');
+                std::cout << "Invalid input. Please enter valid numbers for sides a, b, c" << std::endl;
+                isInputRight = false;
+            }
+            else
+            {
+                std::cin.ignore(32767, '\n');
+                isInputRight = true;
+            }
+        } while (!isInputRight);
+    }
+    void display() const 
+    {
+        if (isValid()) 
+        {
+            std::cout<<"Perimetr: "<<getPerimeter()<<std::endl;
+            std::cout<<"Square: "<<getArea()<<std::endl;
+        }
+        else 
+        {
+            std::cout << "Triangle doesnt exist";
+        }
+    }
+};
+int main() 
+{
+    Triangle t;
+    t.inputSides();
+    t.display();
+    std::cout<<"Side a through indexter: "<<t[0]<<std::endl;
+    std::cout<<"Side b through indexter: "<<t[1]<<std::endl;
+    std::cout<<"Side c through indexter: "<<t[2]<<std::endl;
+    return 0;
+}
---
+++ lab1.2 oop/lab1.2 oop.cpp
@@ -0,0 +1,157 @@
+﻿#include <iostream>
+#include <cmath>
+
+class TETriangle
+{
+protected:
+    double side;
+public:
+    TETriangle() : side(1) {}
+    TETriangle(double s) : side(s) {}
+    TETriangle(const TETriangle& other) : side(other.side) {}
+    double area() const
+    {
+        return (sqrt(3)/4)*side*side;
+    }
+    double perimeter() const
+    {
+        return 3*side;
+    }
+    virtual void output() const
+    {
+        std::cout<<"Triangle with side: "<<side<<std::endl;
+        std::cout<<"Triangle area: "<<area()<<std::endl;
+        std::cout<<"Triangle perimeter: "<<perimeter()<<std::endl;
+    }
+    bool operator==(const TETriangle& other) const
+    {
+        return side == other.side;
+    }
+    TETriangle operator*(double multiplier) const
+    {
+        return TETriangle(side * multiplier);
+    }
+    friend TETriangle operator*(double scalar, const TETriangle& triangle)
+    {
+        return TETriangle(triangle.side * scalar);
+    }
+    double getLength()
+    {
+        return side;
+    }
+};
+class TEPiramid : public TETriangle
+{
+private:
+    double height;
+public:
+    TEPiramid() : TETriangle(), height(1) {}
+    TEPiramid(double s, double h) : TETriangle(s), height(h) {}
+    TEPiramid(const TETriangle& triangle, double h) : TETriangle(triangle), height(h) {}
+    double volume() const
+    {
+        return (1.0/3.0)*area()*height;
+    }
+    void output() const override
+    {
+        std::cout<<"Pyramid with side "<<side<< " and height "<<height<<" :"<<std::endl;
+        std::cout<<"Triangle base area: "<<area()<<std::endl;
+        std::cout<<"Pyramid volume: "<<volume()<<std::endl;
+        std::cout<<"Triangle perimeter (base): "<<perimeter()<<std::endl;
+    }
+    double getHeight() const
+    {
+        return height;
+    }
+    void setHeight(double h)
+    {
+        height = h;
+    }
+};
+int main()
+{
+    //triangle
+    TETriangle triangle; 
+    TETriangle copyTriangle;
+    std::cout<<"How do you want to enter first triangle?"<<std::endl;
+    std::cout<<"1 for no parameter input"<< std::endl;
+    std::cout<<"2 to enter with parameter"<< std::endl;
+    std::cout<<"3 to copy input from another triangle"<< std::endl;
+    int input;
+    std::cin>>input;
+    switch (input)
+    {
+    case 1:
+        std::cout<<"Triangle created with no parameters (side = 0 by default)."<< std::endl;
+        triangle.output();
+        break;
+    case 2:
+        double sideLength;
+        std::cout<<"Enter the side length of the triangle: ";
+        std::cin>>sideLength;
+        if (std::cin.fail())
+        {
+            std::cin.clear();
+            std::cin.ignore(32767, '\n');
+        }
+        triangle = TETriangle(sideLength);
+        triangle.output();
+        break;
+
+    case 3:
+        double copySide;
+        std::cout<<"Enter the side length for the triangle to copy:";
+        std::cin>>copySide;
+        if (std::cin.fail())
+        {
+            std::cin.clear();
+            std::cin.ignore(32767, '\n');
+        }
+        copyTriangle = TETriangle(copySide);
+        triangle = TETriangle(copyTriangle);
+        std::cout<<"Copied triangle:\n";
+        triangle.output();
+        break;
+    default:
+        std::cout<<"Invalid input! Please enter 1, 2, or 3."<<std::endl;
+        return 1; 
+    }
+    std::cout<<"Enter side for second triangle: ";
+    double triangleInput;
+    std::cin>>triangleInput;
+    if (std::cin.fail())
+    {
+        std::cin.clear();
+        std::cin.ignore(32767, '\n');
+    }
+    TETriangle secondTriangle = triangleInput;
+    secondTriangle.output();
+    std::cout<<"\n=== Additional Operations ===\n";
+    if (triangle == secondTriangle)
+        std::cout<<"Triangles are equal."<<std::endl;
+    else
+        std::cout<<"Triangles are not equal."<<std::endl;
+    std::cout<<"Enter number which you want to multiply to triangle: ";
+    double number;
+    std::cin>>number;
+    if (std::cin.fail())
+    {
+        std::cin.clear();
+        std::cin.ignore(32767, '\n');
+    }
+    TETriangle largerTriangle1 = triangle * number;
+    std::cout<<"Triangle after "<<triangle.getLength()<<" * "<<number<<":"<<std::endl;
+    largerTriangle1.output();
+    std::cout<<std::endl;
+    TETriangle largerTriangle2 = number * triangle;
+    std::cout<<"Triangle after "<<number<<" * "<< triangle.getLength()<<":"<<std::endl;
+    largerTriangle2.output();
+    //piramid
+    std::cout<<std::endl;
+    std::cout<<"Enter height for the pyramid: ";
+    double height;
+    std::cin>>height;
+    TEPiramid pyramid(triangle, height);
+    pyramid.output();
+    return 0;
+}
